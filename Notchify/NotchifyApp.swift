41a7572be17f38e8d928bfcb0272ec3f7c5551e3
Kharis <maulanakharis129@gmail.com>
2026-09-21 08:17:08 +0700

    Initial Commit

---
+++ Notchify/NotchifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchifyApp.swift
+//  Notchify
+//
+//  Created by Kharis Destian Maulana on 21/09/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
